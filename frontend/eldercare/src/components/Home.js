592b2f655149213852cf487f625c2f8a3f3a53ea
Osman Warsi <osman.warsi@live.com>
2020-09-05 19:45:28 -0400

    Patient login page

--- frontend/eldercare/src/components/Home.js
+++ frontend/eldercare/src/components/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
-
+import { Button, InputGroup, FormControl, Card, Accordion, Form, Table } from 'react-bootstrap';
 import '../App.css';
 import '../index.css';
-
+import nurse from '../images/2n.jpg';
 
 function Home() {
 
     return(
-        <div>
-            <h1>Medhack 2020</h1>
+        <div className="bodyy"> 
+            <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={nurse} />
+                            <Card.Body>
+                                <Card.Title> <h4> elderCare provides remote treatment for elderly patients</h4> </Card.Title>
+                                <Card.Text>
+                               
+                                </Card.Text>
+                               
+                            </Card.Body>
+                        </Card>
         </div>
     );
 }
